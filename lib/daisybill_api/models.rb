847ba237dc89b2cb916d4c038a53a58ac554c70f
Ben Liscio <bliscio@daisybill.com>
2015-04-13 17:33:11 -0400

    [#88050512] Add ReferringProvider

--- lib/daisybill_api/models.rb
+++ lib/daisybill_api/models.rb
@@ -6,6 +6,7 @@ require 'daisybill_api/models/billing_provider'
 require 'daisybill_api/models/patient'
 require 'daisybill_api/models/injury'
 require 'daisybill_api/models/rendering_provider'
+require 'daisybill_api/models/referring_provider'
 
 module DaisybillApi
   module Models
---
+++ spec/daisybill_api/models/referring_provider_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+
+describe DaisybillApi::Models::ReferringProvider do
+  it_behaves_like DaisybillApi::Ext::Attributes, :id, :first_name, :last_name, :middle_initial, :suffix,
+    :npi, :state_license_number, :active
+  it_behaves_like DaisybillApi::Ext::Attributes::ReadOnly, :id, :billing_provider_id, :created_at, :updated_at
+  it_behaves_like DaisybillApi::Ext::CRUD, :all, :find, :create, :update, '/referring_providers', billing_provider_id: '/billing_providers'
+  it_behaves_like DaisybillApi::Ext::Associations
+  it_behaves_like DaisybillApi::Ext::Links, billing_provider: DaisybillApi::Models::BillingProvider
+end
