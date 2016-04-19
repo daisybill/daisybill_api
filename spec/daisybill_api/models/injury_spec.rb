faa539df7dbae7a6831d38fae014129f7b20141e
Corey Leveen <cleveen@daisybill.com>
2016-04-19 13:59:42 -0400

    Support 1.9.3

--- spec/daisybill_api/models/injury_spec.rb
+++ spec/daisybill_api/models/injury_spec.rb
@@ -1,20 +1,20 @@
 require "spec_helper"
 
 describe DaisybillApi::Models::Injury do
-  EXT_ATTRIBUTES = %i(
-    id
-    adj_number
-    claim_number
-    claims_administrator_id
-    created_at
-    description
-    end_date
-    patient_id
-    payer_id
-    review_status
-    start_date
-    updated_at
-  ).freeze
+  EXT_ATTRIBUTES = [
+    :id,
+    :adj_number,
+    :claim_number,
+    :claims_administrator_id,
+    :created_at,
+    :description,
+    :end_date,
+    :patient_id,
+    :payer_id,
+    :review_status,
+    :start_date,
+    :updated_at,
+  ].freeze
 
   EXT_ATTRIBUTE_TYPES = {
     claims_administrator: :model,
@@ -25,19 +25,19 @@ describe DaisybillApi::Models::Injury do
     review_errors:   :collection
   }.freeze
 
-  EXT_SEND_AS_ATTRIBUTES = %i(
-    adj_number
-    claim_number
-    claims_administrator_id
-    description
-    employer_attributes
-    end_date
-    patient_id
-    payer_id
-    start_date
-  ).freeze
+  EXT_SEND_AS_ATTRIBUTES = [
+    :adj_number,
+    :claim_number,
+    :claims_administrator_id,
+    :description,
+    :employer_attributes,
+    :end_date,
+    :patient_id,
+    :payer_id,
+    :start_date
+  ].freeze
 
-  it_behaves_like DaisybillApi::Ext::Attributes, *EXT_ATTRIBUTES, **EXT_ATTRIBUTE_TYPES
+  it_behaves_like DaisybillApi::Ext::Attributes, *EXT_ATTRIBUTES, EXT_ATTRIBUTE_TYPES
   it_behaves_like DaisybillApi::Ext::Attributes::SendAs, *EXT_SEND_AS_ATTRIBUTES
 
   it_behaves_like DaisybillApi::Ext::CRUD, :all, :find, :create, :update, :destroy, "/injuries", patient_id: "/patients"
