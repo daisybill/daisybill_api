887f5f2b9a10e809f55315019828106f43d216b5
Ruslan Milevskiy <rpmilevskiy@gmail.com>
2017-01-04 15:17:24 +0200

    [#136963781] Add RequestForSecondReview model

---
+++ spec/daisybill_api/models/disputed_service_spec.rb
@@ -0,0 +1,8 @@
+require 'spec_helper'
+
+describe DaisybillApi::Models::DisputedService do
+  it_behaves_like DaisybillApi::Ext::Attributes, :id, :service_line_item_id, :reason, :authorized,
+    :supporting_documentation_attached, :created_at, :updated_at
+  it_behaves_like DaisybillApi::Ext::Attributes::SendAs, :service_line_item_id, :reason, :authorized,
+    :supporting_documentation_attached
+end
