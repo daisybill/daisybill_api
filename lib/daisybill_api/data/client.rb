6bf27a51d2c9cc7defc9b7701256ca92612ed9da
Darren Johnson <darrenbjohnson@gmail.com>
2018-04-19 09:42:45 -0700

    remove content-type header entirely

--- lib/daisybill_api/data/client.rb
+++ lib/daisybill_api/data/client.rb
@@ -28,8 +28,7 @@ module DaisybillApi
         data = {
           method: method,
           url: url,
-          payload: params,
-          headers: { "Content-Type" => "application/json" }
+          payload: params
         }
         RestClient::Request.execute(data) { |response, request, status|
           @headers = response.headers
